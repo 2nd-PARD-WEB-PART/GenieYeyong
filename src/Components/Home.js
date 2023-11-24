c8520f57219ce8cdca23cfcbe993f7b4f5e02ea6
Cocomong98 <yonghjjm0618@gmail.com>
2023-11-24 23:46:24 +0900

    temp

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,26 @@
+/* disable-eslint */
+
 import React, { useEffect } from "react";
+import HomeProblem from "./HomeProblem";
 import styled, { keyframes,css } from 'styled-components';
 import { useState } from "react";
 // Keyframes for moving up animation
 
+const WishList = [
+    {
+
+    },
+    {
+
+    },
+]
 
 const Home=()=>{
-
-    
-
-    return(<>
-    </>)
-
+    return(
+        <>
+            Hello
+        </>
+    )
 }
 
 
